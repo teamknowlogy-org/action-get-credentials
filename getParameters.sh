06269907c27cdd44728486fa1eca7acaaad7feaf
gonzape27 <gonza27@gmail.com>
2023-12-02 09:48:26 -0300

    add new filter

--- getParameters.sh
+++ getParameters.sh
@@ -13,16 +13,19 @@ do
 	export SSM_GLOBAL_PATH="$SSM_BASE_PATH/global"
 	export SSM_PATH="$SSM_GLOBAL_PATH"
         echo "GETTING GLOBAL PARAMETERS FOR $SSM_PATH"
+	echo "#GLOBAL PARAMETERS" >> .env
+
 	for i in $(aws ssm get-parameters-by-path --region $region --recursive --path "$SSM_PATH" | jq -c '.Parameters[] | .Name + "=" + .Value' |rev | cut -d"/" -f1 | rev |  sed  's/"//g')
 	do
 		echo "$i" >> .env
 	done
 	
 	## FETCH SPECIFIC VALUES
-        export SSM_SERVICE_PATH="$SSM_BASE_PATH/service/$x"
+        echo "#SPECIFIC PARAMETERS" >> .env
+        export SSM_SERVICE_PATH="$SSM_BASE_PATH/services/$x"
         export SSM_PATH="$SSM_SERVICE_PATH"
 	echo "GETTING APPLICATION PARAMETERS FOR $SSM_PATH"
-	for i in $(aws ssm get-parameters-by-path --region $region --recursive --path "$SSM_PATH" | jq -c '.Parameters[] | .Name + "=" + .Value' |rev | cut -d"/" -f1 | rev |  sed  's/"//g')
+	for i in $(aws ssm get-parameters-by-path --region $region --recursive --path "$SSM_PATH" | jq -c '.Parameters[] | .Name + "=" + .Value'  |  sed  's/"//g' | cut -d "/"  -f6- | awk '{print $0}')
 	do
 		echo "$i" >> .env
 	done 
